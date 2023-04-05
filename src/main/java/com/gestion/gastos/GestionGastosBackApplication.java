7f37ee08ca5867d4f6ea2add9d1b9835d7dda65b
Cesar Estiven Medrano <cesar-2007-mesa@hotmail.com>
2023-04-05 17:08:23 -0500

    Inicio del proyecto

---
+++ src/main/java/com/gestion/gastos/GestionGastosBackApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionGastosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionGastosBackApplication.class, args);
+	}
+
+}
