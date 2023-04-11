9429b2a02431392f4f042ac64b7ee538bab71320
Cesar Estiven Medrano <cesar-2007-mesa@hotmail.com>
2023-04-10 23:51:08 -0500

    Esquema de usuario - rol

---
+++ src/main/java/com/gestion/gastos/entidades/Usuario.java
@@ -0,0 +1,38 @@
+package com.gestion.gastos.entidades;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+    private String password;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private int edad;
+    private String perfil;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "usuario")
+    private Set<UsuarioRol> UsuarioRol = new HashSet<>();
+
+}
